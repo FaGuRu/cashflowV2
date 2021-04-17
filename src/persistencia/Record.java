01d643c24d18bdf68dc5c2c2726b15e20edf2b5c
FaGuRu <193247@ids.upchiapas.edu.mx>
2021-04-16 20:07:24 -0500

    Clase y mapping de Record creado.

---
+++ src/persistencia/Record.java
@@ -0,0 +1,84 @@
+package persistencia;
+
+import java.util.Date;
+
+public class Record {
+    int id;
+    String type;
+    int week_num;
+    Date month;
+    String company_name;
+    float amount;
+
+    public Record() {
+    }
+
+    public Record(int id, String type, int week_num, Date month, String company_name, float amount) {
+        this.id = id;
+        this.type = type;
+        this.week_num = week_num;
+        this.month = month;
+        this.company_name = company_name;
+        this.amount = amount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getWeek_num() {
+        return week_num;
+    }
+
+    public void setWeek_num(int week_num) {
+        this.week_num = week_num;
+    }
+
+    public Date getMonth() {
+        return month;
+    }
+
+    public void setMonth(Date month) {
+        this.month = month;
+    }
+
+    public String getCompany_name() {
+        return company_name;
+    }
+
+    public void setCompany_name(String company_name) {
+        this.company_name = company_name;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Record{" +
+                "id=" + id +
+                ", type='" + type + '\'' +
+                ", week_num=" + week_num +
+                ", month=" + month +
+                ", company_name='" + company_name + '\'' +
+                ", amount=" + amount +
+                '}';
+    }
+}
