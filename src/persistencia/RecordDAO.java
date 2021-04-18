2a871c5429918d1e9d0591f43d8bae6780e2254f
LuisFelipe333 <luigifelipe333@gmail.com>
2021-04-18 14:53:57 -0500

    Agregado RecordDAO y conexión con SQL

---
+++ src/persistencia/RecordDAO.java
@@ -0,0 +1,92 @@
+package persistencia;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Restrictions;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Date;
+
+
+public class RecordDAO {
+
+    private static SessionFactory factory;
+    private static ServiceRegistry serviceRegistry;
+
+    public RecordDAO(String manejador) {
+        System.err.println("Iniciando");
+        try {
+            Configuration configuration = new Configuration();
+            System.err.println("Leyendo configuracion.");
+            configuration.configure(manejador);
+            serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
+            factory = configuration.buildSessionFactory(serviceRegistry);
+            System.out.println("Exito");
+        } catch (Throwable ex) {
+            System.err.println("No se puede crear la Sesion manejador 1" + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+
+    }
+
+    public void addRecord(Record c1)
+    {
+        Session session=factory.openSession();
+        session.beginTransaction();
+        session.save(c1);
+
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    public List getRecord(){
+        Session session=factory.openSession();
+        Criteria cr= session.createCriteria(Record.class);
+        List empList1= cr.list();
+        return empList1;
+    }
+
+    public List getMonthRecord(int numberMonth)
+    {
+        String sql = "SELECT id, type, week, month, company_name, amount FROM record\n"+
+                "WHERE MONTH(month)="+numberMonth;
+
+        Connection connection = SQLConnection.getConnection();
+        List<Record> Records = new ArrayList<>();
+        try {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            ResultSet results = statement.executeQuery();
+            while (results.next()) {
+                int id = results.getInt(1);
+                String type = results.getString(2);
+                int week = results.getInt(3);
+                Date month = results.getDate(4);
+                String companyName = results.getString(5);
+                float amount = results.getFloat(6);
+                Record record = new Record(id, type, week, month, companyName, amount);
+                Records.add(record);
+            }
+
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        System.out.println(Records);
+        return Records;
+
+    }
+
+
+
+}
