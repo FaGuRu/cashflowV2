70a901b715b21b6f0758be15ad412065dfd2fe17
LuisFelipe333 <luigifelipe333@gmail.com>
2021-04-19 04:09:08 -0500

    Agregado PG Connection

---
+++ src/persistencia/PGConnection.java
@@ -0,0 +1,38 @@
+package persistencia;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+
+public class PGConnection {
+    private static PGConnection instancia =null;
+    private static Connection connection =null;
+
+    public static PGConnection getInstance() {
+        if(instancia==null) {
+            instancia = new PGConnection();
+            instancia.Connection();
+        }
+        return instancia;
+    }
+
+    public void Connection() {
+        String connectionString ="jdbc:postgresql://localhost:5432/BDADM";
+        String user = "postgres";
+        String password = "Sword0Shield155";
+        try {
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection(connectionString,user,password);
+            if(connection!=null) {
+                System.out.println("Conexion Postgres exitosa");
+            }else {
+                System.out.println("Conexion Postgres fallida");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+    public Connection getConnection() {
+        return connection;
+    }
+
+}
